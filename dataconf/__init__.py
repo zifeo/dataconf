e26cfcaf6b0bdeda11e7c23ec92be0caaa45f6bb
Dustin Smith <dustin.smith@truedigital.com>
2021-08-25 08:45:09 +0700

    created version.py for __version__ which will populate from the library toml; incremented to 0.1.6

--- dataconf/__init__.py
+++ dataconf/__init__.py
@@ -2,5 +2,6 @@ from dataconf.utils import dump
 from dataconf.utils import dumps
 from dataconf.utils import load
 from dataconf.utils import loads
+from dataconf.version import __version__
 
 __all__ = ["load", "loads", "dump", "dumps"]
